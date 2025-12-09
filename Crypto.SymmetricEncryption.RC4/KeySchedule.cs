c5d2d67c1dab211a1ea0aab2e411e830d0c77ea7
Evening Doggie <04.sunrise_picnics@icloud.com>
2025-12-09 23:03:43 +0300

    RC4 implemented

---
+++ Crypto.SymmetricEncryption.RC4/KeySchedule.cs
@@ -0,0 +1,51 @@
+﻿namespace Crypto.SymmetricEncryption.RC4;
+
+public sealed partial class RC4
+{
+    internal class KeySchedule
+    {
+        #region Fields
+
+        private readonly byte[] _sBox = new byte[BoxSize];
+
+        private const int BoxSize = 256;
+
+        #endregion
+
+
+        #region Properties
+
+        public byte[] SBox => _sBox;
+
+        #endregion
+
+
+        #region Methods
+
+        public byte[] Expand(byte[] key)
+        {
+            var kBox = new byte[BoxSize];
+
+            for (var i = 0; i < BoxSize; ++i)
+            {
+                _sBox[i] = (byte)i;
+            }
+
+            for (var i = 0; i < BoxSize; ++i)
+            {
+                kBox[i] = key[i % key.Length];
+            }
+
+            var j = 0;
+            for (var i = 0; i < BoxSize; ++i)
+            {
+                j = (j + _sBox[i] + kBox[i]) % BoxSize;
+                (_sBox[i], _sBox[j]) = (_sBox[j], _sBox[i]);
+            }
+
+            return _sBox;
+        }
+
+        #endregion
+    }
+}
---
+++ Crypto.SymmetricEncryption.RC4/RC4.cs
@@ -0,0 +1,180 @@
+﻿using Crypto.Core.Base.Interfaces;
+
+namespace Crypto.SymmetricEncryption.RC4;
+
+public sealed partial class RC4 :
+    IEncryptionAlgorithm
+{
+    #region Fields
+
+    private readonly KeySchedule _keySchedule 
+        = new();
+    
+    private readonly PseudoRandomGenerationAlgorithm _prga 
+        = new();
+    
+    #endregion
+    
+    
+    #region Constructors
+
+    public RC4(byte[] key)
+    {
+        _keySchedule.Expand(key);
+    }
+    
+    #endregion
+    
+    
+    #region Methods
+    
+    public Memory<byte> Encrypt(
+        Memory<byte> data)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+        
+        if (data.Length == 0)
+            return data;
+        
+        var span = data.Span;
+        
+        for (var i = 0; i < data.Length; ++i)
+        {
+            span[i] ^= _prga.GetNextByte(_keySchedule.SBox);
+        }
+
+        return data;
+    }
+
+    public Memory<byte> Decrypt(
+        Memory<byte> data)
+    {
+        return Encrypt(data);
+    }
+
+    public void Encrypt(
+        byte[] data,
+        out byte[] result)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+        
+        if (data.Length == 0)
+            result = data;
+        
+        result = new byte[data.Length];
+        
+        for (var i = 0; i < data.Length; ++i)
+        {
+            result[i] = (byte)(
+                data[i] ^ 
+                _prga.GetNextByte(_keySchedule.SBox));
+        }
+    }
+
+    public void Decrypt(
+        byte[] data,
+        out byte[] result)
+    {
+        Encrypt(data, out result);
+    }
+
+    public void Encrypt(
+        string inputFilePath, 
+        string outputFilePath)
+    {
+        if (string.IsNullOrWhiteSpace(inputFilePath))
+            throw new ArgumentException("Input file path cannot be null or empty.", nameof(inputFilePath));
+        
+        var buffer = new byte[1];
+
+        using var inputStream = 
+            new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
+        using var outputStream = 
+            new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
+    
+        while (inputStream.Read(buffer) > 0)
+        {
+            buffer[0] ^= _prga.GetNextByte(_keySchedule.SBox);
+            
+            outputStream.Write(
+                buffer, 
+                0, 
+                1);
+        }
+    }
+
+    public void Decrypt(
+        string inputFilePath, 
+        string outputFilePath)
+    {
+        Encrypt(inputFilePath, outputFilePath);
+    }
+
+    public async Task<byte[]> EncryptAsync(
+        byte[] data,
+        CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+
+        if (data.Length == 0)
+            return data;
+
+        await Parallel.ForAsync(
+            0,
+            data.Length, 
+            cancellationToken,
+            (i, token) =>
+            {
+                token.ThrowIfCancellationRequested();
+                
+                data[i] ^= _prga.GetNextByte(
+                    _keySchedule.SBox);
+                
+                return ValueTask.CompletedTask;
+            });
+        
+        return data;
+    }
+
+    public async Task<byte[]> DecryptAsync(
+        byte[] data,
+        CancellationToken cancellationToken = default)
+    {
+        return await EncryptAsync(data, cancellationToken);
+    }
+
+    public async Task EncryptAsync(
+        string inputFilePath,
+        string outputFilePath,
+        CancellationToken cancellationToken = default)
+    {
+        if (string.IsNullOrWhiteSpace(inputFilePath))
+            throw new ArgumentException("Input file path cannot be null or empty.", nameof(inputFilePath));
+        
+        var buffer = new byte[1];
+
+        await using var inputStream = 
+            new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
+        await using var outputStream = 
+            new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
+    
+        while (await inputStream.ReadAsync(buffer, cancellationToken) > 0)
+        {
+            buffer[0] ^= _prga.GetNextByte(_keySchedule.SBox);
+            
+             await outputStream.WriteAsync(
+                 buffer.AsMemory(0, 1), 
+                 cancellationToken);
+        }
+    }
+
+    public async Task DecryptAsync(
+        string inputFilePath,
+        string outputFilePath,
+        CancellationToken cancellationToken = default)
+    {
+        await EncryptAsync(inputFilePath, outputFilePath, cancellationToken);
+    }
+    
+    #endregion
+}
